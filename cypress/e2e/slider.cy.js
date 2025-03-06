249ffe38f57e150568b9f773378aa50f8e0d98d4
Piotr Świebocki <swpiotr5@interia.pl>
2025-03-06 08:32:14 +0100

    cypress and hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,25 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Responsive Swiper Gallery Test', function () {
+  const devices = [
+    { name: 'Macbook-16', width: 1536, height: 960 },
+    { name: 'iPad', width: 768, height: 1024 },
+    { name: 'iPhone-12', width: 390, height: 844 }
+  ];
+
+  devices.forEach((device) => {
+    it(`Checks gallery responsiveness on ${device.name}`, function () {
+      cy.viewport(device.width, device.height);
+      cy.visit('http://localhost:3000');
+
+      cy.get('.swiper').should('be.visible');
+
+      cy.get('.swiper-button-next').should('be.visible').click();
+      cy.get('.swiper-button-prev').should('be.visible').click();
+
+      cy.get('.swiper-slide-active').should('exist');
+    });
+  });
+});
